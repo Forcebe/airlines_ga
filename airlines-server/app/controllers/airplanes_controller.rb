43bdb3cf9367d353dcdc8052f0f56c575b34142f
mhulbert85 <38045450+mhulbert85@users.noreply.github.com>
2020-11-03 19:30:39 +1100

    Create: Plane header

--- airlines-server/app/controllers/airplanes_controller.rb
+++ airlines-server/app/controllers/airplanes_controller.rb
@@ -1,39 +1,40 @@
 class AirplanesController < ApplicationController
   def index
-  @airplanes = Airplane.all
-end
+    @airplanes = Airplane.all
+  end
 
-def new
-  @airplane = Airplane.new
-end
+  def new
+    @airplane = Airplane.new
+  end
 
-def create
-  airplane = Airplane.create airplane_params
-  redirect_to airplane
-end
+  def create
+    airplane = Airplane.create airplane_params
+    redirect_to airplane
+  end
 
-def show
-  @airplane = Airplane.find params[:id]
-end
+  def show
+    @airplane = Airplane.find params[:id]
+  end
 
-def edit
-  @airplane = Airplane.find params[:id]
-end
+  def edit
+    @airplane = Airplane.find params[:id]
+  end
 
-def update
-  airplane = Airplane.find params[:id]
-  airplane.update airplane_params
-  redirect_to airplane
-end
+  def update
+    airplane = Airplane.find params[:id]
+    airplane.update airplane_params
+    redirect_to airplane
+  end
 
-def destroy
-  airplane = Airplane.find params[:id]
-  airplane.destroy
-  redirect_to airplanes_path # index
-end
+  def destroy
+    airplane = Airplane.find params[:id]
+    airplane.destroy
+    redirect_to airplanes_path # index
+  end
 
-private
-def airplane_params
-  params.require(:airplane).permit(:name, :rows, :columns)
-end
+  private
+
+  def airplane_params
+    params.require(:airplane).permit(:name, :rows, :columns)
+  end
 end
