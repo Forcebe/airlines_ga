773b83a1c902a1373eed38cd19defc75f7002261
lightgreener <woniora@hotmail.com>
2020-11-03 16:31:24 +1100

    app.js done

--- airlines-client/src/components/App.js
+++ airlines-client/src/components/App.js
@@ -1,15 +1,10 @@
-<<<<<<< HEAD
+
 
 function App() {
   return (
     <div className="App">
         <h1> Test React </h1>
-=======
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
->>>>>>> 1513a9f925606afc4ba6b3b36d9d11cc0489ab2e
+
     </div>
   );
 }
