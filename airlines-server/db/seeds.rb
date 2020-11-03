0e6046c84dde05cb4b52a4335d81e6a16da6ea57
mhulbert85 <38045450+mhulbert85@users.noreply.github.com>
2020-11-03 18:16:30 +1100

    Update: Seed data

--- airlines-server/db/seeds.rb
+++ airlines-server/db/seeds.rb
@@ -1,12 +1,12 @@
 User.destroy_all
-u1 = User.create :email => 'nick@ga.com', :is_admin => true
-u2 = User.create :email => 'mike@ga.com', :is_admin => true
-u3 = User.create :email => 'henry@ga.com', :is_admin => true
+u1 = User.create :email => 'nick@ga.com', :name => 'Nick', :password => 'chicken', :is_admin => true
+u2 = User.create :email => 'mike@ga.com', :name => 'Mike', :password => 'chicken', :is_admin => true
+u3 = User.create :email => 'henry@ga.com', :name => 'Henry', :password => 'chicken',  :is_admin => true
 puts "#{ User.count } users"
 
 Airplane.destroy_all
 
-a1 = Airplane.create :name => 'GA123', :rows => 'A,B,C,D', :columns => 20
-a2 = Airplane.create :name => 'GA456', :rows => 'A,B,C', :columns => 22
-a3 = Airplane.create :name => 'GA789', :rows => 'A,B,C,D,E,F', :columns => 20
+a1 = Airplane.create :name => 'GA123', :rows => 4, :columns => 20
+a2 = Airplane.create :name => 'GA456', :rows => 5, :columns => 22
+a3 = Airplane.create :name => 'GA789', :rows => 6, :columns => 20
 puts "#{ Airplane.count } airplanes"
