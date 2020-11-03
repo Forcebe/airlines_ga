0e3f34d16a8366d0e9b6b9589d4d2b1affe8fc5a
Nick Mather <nickmather12@gmail.com>
2020-11-04 10:57:41 +1100

    login component underway

--- airlines-client/src/components/App.js
+++ airlines-client/src/components/App.js
@@ -1,10 +1,12 @@
 import Airplanes from './Airplanes';
+import Login from './Login';
 
 function App() {
   return (
     <div className="App">
         <h1> Test React </h1>
         < Airplanes />
+        < Login />
 
     </div>
   );
--- airlines-client/src/components/Login.js
+++ airlines-client/src/components/Login.js
@@ -3,8 +3,52 @@ import React, { Component } from 'react';
 class Login extends Component {
   render() {
     return(
-      <h1> Login page coming soon</h1>
+      <div>
+        <h1> Log In</h1>
+        < LoginForm />
+      </div>
     )
   }
 }
-export default Airplanes;
+
+class LoginForm extends Component {
+  constructor () {
+    super();
+    this.state = {
+      email: '',
+      password: ''
+    }
+    this._handleEmail = this._handleEmail.bind(this)
+    this._handlePassword = this._handlePassword.bind(this)
+  }
+
+  _handleEmail(e) {
+    this.setState( { email: e.target.value})
+
+  }
+
+  _handlePassword(e) {
+    this.setState( { password: e.target.value})
+  }
+
+  render() {
+    return (
+      <form>
+        <label>Email</label>
+        <input
+            onChange={this._handleEmail}
+            autoFocus
+            type="email"
+          />
+          <label>Password</label>
+          <input
+            onChange={this._handlePassword}
+            type="password"
+          />
+        <input type="submit" value="Login" />
+      </form>
+    )
+  }
+}
+
+export default Login;
