f871ae11bb2ada7283c43478063e4fc0eadfd252
mhulbert85 <38045450+mhulbert85@users.noreply.github.com>
2020-11-04 12:46:42 +1100

    Update: react router

--- airlines-client/src/components/App.js
+++ airlines-client/src/components/App.js
@@ -5,6 +5,7 @@ import Login from './Login';
 import Home from './Home';
 import Admin from './Admin';
 import Plane from './Plane';
+import Flight from './Flight';
 
 function App() {
   return (
@@ -12,16 +13,20 @@ function App() {
         <h1> Boring Airlines </h1>
         <Router>
             <nav>
-             <Link to="/">Home</Link>
-             <Link to="/login">Login</Link>
-             <Link to="/admin">Admin</Link>
-             <Link to="/plane">Planes</Link>
+               <ul>
+                 <li><Link to="/">Home</Link></li>
+                 <li><Link to="/login">Login</Link></li>
+                 <li><Link to="/admin">Admin</Link></li>
+                 <li><Link to="/plane">Planes</Link></li>
+                 <li><Link to="/flight">Flights</Link></li>
+             </ul>
            </nav>
            <Switch>
               <Route exact path="/" component={Home}/>
               <Route exact path="/login" component={Login}/>
               <Route exact path="/admin" component={Admin}/>
               <Route exact path="/plane" component={Plane}/>
+              <Route exact path="/flight" component={Flight}/>
            </Switch>
         </Router>
     </div>
