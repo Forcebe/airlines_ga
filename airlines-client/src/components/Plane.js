af8327e8b61eeef9fad54ab1130314de40eebda0
mhulbert85 <38045450+mhulbert85@users.noreply.github.com>
2020-11-04 16:50:37 +1100

    update header

--- airlines-client/src/components/Plane.js
+++ airlines-client/src/components/Plane.js
@@ -1,32 +1,10 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import '../plane.css';
-
-
-import Admin from './Admin';
-import Flight from './Flight';
+import '../stylesheets/plane.css';
 
 class Plane extends Component {
   render() {
     return (
       <div>
-       <header>
-        <div className="container">
-          <Router>
-              <nav>
-                 <ul>
-                  <li><Link to="">Planes</Link></li>
-                  <li><Link to="/flight">Flights</Link></li>
-                  <li><Link to="/admin">Admin</Link></li>
-                 </ul>
-              </nav>
-             <Switch>
-                <Route exact path="/flight" component={Flight}/>
-                <Route exact path="/admin" component={Admin}/>
-             </Switch>
-          </Router>
-        </div>
-       </header>
        <div className="form">
           <div className="grid-col-2">
               <h1 className="title"> Boring Airlines: Planes </h1>
