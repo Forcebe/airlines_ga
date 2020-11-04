d0ab207640bf3b0223baa7aae622b4d083f8c995
mhulbert85 <38045450+mhulbert85@users.noreply.github.com>
2020-11-04 16:09:59 +1100

    page layout

--- airlines-client/src/components/Plane.js
+++ airlines-client/src/components/Plane.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import '../plane.css';
+
 
 import Admin from './Admin';
 import Flight from './Flight';
@@ -7,10 +9,9 @@ import Flight from './Flight';
 class Plane extends Component {
   render() {
     return (
-      <div className="container">
-          <h1> Planes </h1>
-          <button>Create Plane</button>
-          <div className="create-plane"> </div>
+      <div>
+       <header>
+        <div className="container">
           <Router>
               <nav>
                  <ul>
@@ -24,7 +25,24 @@ class Plane extends Component {
                 <Route exact path="/admin" component={Admin}/>
              </Switch>
           </Router>
-      </div>
+        </div>
+       </header>
+       <div className="form">
+          <div className="grid-col-2">
+              <h1 className="title"> Boring Airlines: Planes </h1>
+              <button className="btn-primary">Create Plane</button>
+           </div>
+           <div>
+            <form className="grid-col-3">
+                <input type="text" placeholder="name" className="form-control row-1 col-1"/>
+                <input type="number" placeholder="row" className="form-control row-1 col-2"/>
+                <input type="number" placeholder="columns" className="form-control  row-1 col-3"/>
+                <input type="submit" value="Cancel" className="btn-primary row-2 col-2"/>
+                <input type="submit" value="Save" className="btn-secondary row-2 col-3"/>
+            </form>
+           </div>
+        </div>
+     </div>
     );
   }
 }
