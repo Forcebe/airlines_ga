2b4e50832f869e8d37689c41ed26b3a61b305781
mhulbert85 <38045450+mhulbert85@users.noreply.github.com>
2020-11-04 12:40:04 +1100

    Create: react routes

--- airlines-client/src/components/App.js
+++ airlines-client/src/components/App.js
@@ -1,13 +1,29 @@
-import Airplanes from './Airplanes';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 import Login from './Login';
+import Home from './Home';
+import Admin from './Admin';
+import Plane from './Plane';
 
 function App() {
   return (
     <div className="App">
-        <h1> Test React </h1>
-        < Airplanes />
-        < Login />
-
+        <h1> Boring Airlines </h1>
+        <Router>
+            <nav>
+             <Link to="/">Home</Link>
+             <Link to="/login">Login</Link>
+             <Link to="/admin">Admin</Link>
+             <Link to="/plane">Planes</Link>
+           </nav>
+           <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/login" component={Login}/>
+              <Route exact path="/admin" component={Admin}/>
+              <Route exact path="/plane" component={Plane}/>
+           </Switch>
+        </Router>
     </div>
   );
 }
