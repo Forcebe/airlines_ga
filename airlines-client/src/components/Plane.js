d93eb80c52d660dcb0c5c19cb99324b207dda7ab
mhulbert85 <38045450+mhulbert85@users.noreply.github.com>
2020-11-05 09:31:40 +1100

    style flights

--- airlines-client/src/components/Plane.js
+++ airlines-client/src/components/Plane.js
@@ -9,8 +9,8 @@ class Planes extends Component {
           <div className="grid-col-3">
               <h1 className="title col-1"> Aeroplanes </h1>
               <button className="btn-primary col-3 h45">Create Plane</button>
-           </div>
-           <div>
+          </div>
+          <div>
             <form onSubmit={ this._handleSubmit } className="grid-col-3">
                 <input onChange={ this._handleChange } type="text" placeholder="name" className="form-control row-1 col-1"/>
                 <input type="number" placeholder="row" className="form-control row-1 col-2"/>
@@ -18,8 +18,8 @@ class Planes extends Component {
                 <input type="submit" value="Cancel" className="btn-primary row-2 col-2"/>
                 <input type="submit" value="Save" className="btn-primary row-2 col-3"/>
             </form>
-           </div>
-        </div>
+          </div>
+         </div>
      </div>
     );
   }
