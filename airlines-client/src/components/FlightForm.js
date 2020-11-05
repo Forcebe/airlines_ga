dd6a2b5dac9a85730df561dfce878815bbd8c2f2
Nick Mather <nickmather12@gmail.com>
2020-11-05 12:26:08 +1100

    form created

--- airlines-client/src/components/FlightForm.js
+++ airlines-client/src/components/FlightForm.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import FlightResults from './FlightResults'
 
 class FlightForm extends Component {
+  constructor(){
+    super();
+    this.state = { content: ''};
+  }
+  _handleChange = () => {
+    console.log('a change')
+  }
   render () {
     return (
-      <h1> Flight form coming soon </h1>
+      <div className="section">
+        <div className="form">
+          <div className="grid-col-1">
+            <h1 className="title col-1 row-1"> Flight Search </h1>
+          </div>
+          <form className="grid-col-2">
+              <input type="search" placeholder=" from " required className="form-control row-1 col-1" />
+                 <input type="search" placeholder=" to " required className="form-control  row-1 col-2" />
+                 <input type="submit" className="btn-primary row-2 col-2"/>
+          </form>
+          <FlightResults />
+        </div>
+      </div>
     )
   }
 }
--- airlines-client/src/components/Home.js
+++ airlines-client/src/components/Home.js
@@ -1,13 +1,9 @@
 import React from 'react';
-<<<<<<< HEAD
 import axios from 'axios'
 import {Link} from 'react-router-dom'
 import FlightForm from './FlightForm'
-import FlightResults from './FlightResults'
-=======
-import axios from 'axios';
-import {Link} from 'react-router-dom';
->>>>>>> d338b261d5e0ed78408dfe325ad29cf3f26f65cd
+
+
 
 const Home = (props) => {
   const handleClick = () => {
@@ -21,7 +17,6 @@ const Home = (props) => {
 
   return (
     <div>
-      <h1>Flight Search</h1>
       {
         props.loggedInStatus ?
         <p>Logged in as {props.user && props.user.name}</p> : null
@@ -30,10 +25,10 @@ const Home = (props) => {
         props.loggedInStatus ?
         <Link to='/logout' onClick={handleClick}>Log Out</Link> : null
       }
-
-      <FlightForm />
-      <FlightResults />
-
+      {
+        props.loggedInStatus ?
+        <FlightForm /> : null
+      }
     </div>
   )
 }
