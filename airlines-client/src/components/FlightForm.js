868d8d71153b4f05c18f4a47b76c65d6eae5f273
Nick Mather <nickmather12@gmail.com>
2020-11-05 13:31:32 +1100

    flight search form commenced, got change handler working

--- airlines-client/src/components/FlightForm.js
+++ airlines-client/src/components/FlightForm.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import FlightResults from './FlightResults'
 
 class FlightForm extends Component {
-  constructor(){
-    super();
-    this.state = { content: ''};
+  constructor(props){
+    super(props);
+    this.state = {
+      origin: '',
+      destination: ''
+    };
   }
-  _handleChange = () => {
-    console.log('a change')
-  }
+  _handleChange = (event) => {
+    const {name, value} = event.target
+    this.setState({
+      [name]: value
+    })
+  };
   render () {
+    const {origin, destination} = this.state
+
     return (
       <div className="section">
         <div className="form">
@@ -17,8 +25,23 @@ class FlightForm extends Component {
             <h1 className="title col-1 row-1"> Flight Search </h1>
           </div>
           <form className="grid-col-2">
-              <input type="search" placeholder=" from " required className="form-control row-1 col-1" />
-                 <input type="search" placeholder=" to " required className="form-control  row-1 col-2" />
+              <input
+                placeholder="from"
+                type="text"
+                name="origin"
+                value={origin}
+                onChange={this._handleChange}
+                className="form-control row-1 col-1"
+              />
+              <input
+                type="search"
+                onChange={this._handleChange}
+                placeholder=" to "
+                name="destination"
+                value={destination}
+                required
+                className="form-control  row-1 col-2"
+              />
                  <input type="submit" className="btn-primary row-2 col-2"/>
           </form>
           <FlightResults />
